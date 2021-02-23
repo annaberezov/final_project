8cdfa520f8ccf5cdaea9fdd8aa6419d7f89eb25d
Anna-Maria Berezovski <ab477869@sju.edu>
2021-02-23 07:58:02 -0500

    Basic NLP page
    Added some information to basic nlp.  Did overall styling changes

--- src/components/Basic.js
+++ src/components/Basic.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
 
 import './App.css';
 
@@ -23,7 +24,19 @@ class Basic extends React.Component {
         Basic NLP
       </Container>
       <Container className='App-body'>
-        Hello
+      <Image src="https://c1.sfdcstatic.com/content/dam/blogs/ca/Blog%20Posts/get-to-know-ai-for-business-natural-language-processing-open-graph.png" fluid rounded />
+        <Container className='App-text'>
+          Natural Language Processing, better known as NLP is a subfield combining computer science and linguistics. 
+          It can be categorized as a form of artificial intelligence focused on human language. The big question of NLP is how can we train machines to understand
+          and communicate with language just like humans do. Some categories of NLP include speech recognition, text-to-speech, part-of-speech tagging, parsing of sentences, 
+          understanding of semantics, machine translation, and many others! NLP is a very large subcategory of AI which encompasses any and all use of human language. 
+        </Container>
+        <Image src="https://miro.medium.com/max/768/1*z_EFTnhzxA3FJROcjOqNXg.png" fluid rounded />
+        <Container className='App-text'>
+          Common applications of NLP include language translators such as Google translate and chatbots. 
+          NLP also interacts with other fields of computer science such as computer vision; within Google translate for example, a picture can be used as the base for translation. 
+          The machine must first identify the letters and text before translating. There are many applications of NLP across many different fields.
+        </Container>        
       </Container>
       </>
   );
